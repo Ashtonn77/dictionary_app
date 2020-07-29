a8a799db161950980a62e19ac5f5159ec3b949c2
Ashtonn77 <ashtonrbay@gmail.com>
2020-07-29 18:54:55 +0200

    added similar word check if user enters an incorrect word thats similar to another word

--- app.py
+++ app.py
@@ -6,20 +6,33 @@ data = json.load(open('data.json'))
 
 def get_definition(word):
     word = word.lower()
+
     if word in data:
         return data[word]
+
     elif len(get_close_matches(word, data.keys())) > 0:
         ans = input("Did you mean {} instead? Enter Y for yes, or N for No: ".format(
             get_close_matches(word, data.keys())[0]))
+
         if ans.upper() == 'Y':
             return data[get_close_matches(word, data.keys())[0]]
         elif ans.upper() == 'N':
             return "We don't seem to have a definition for that word. Try another"
         else:
             return "Sorry we don't understand that input"
+
     else:
         return "We don't seem to have a definition for that word. Try another"
 
 
 word = input('Enter word__')
-print(get_definition(word))
+
+output = get_definition(word)
+
+if type(output) == list:
+
+    for item in output:
+        print(item)
+
+else:
+    print(output)
